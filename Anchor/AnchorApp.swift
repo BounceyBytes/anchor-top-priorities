c94348bc52e4e1ea0b47dca7c5a455b14b9c6132
BounceyBytes <birney.james@gmail.com>
2025-12-24 16:20:45 -0800

    Initial Commit

---
+++ Anchor/AnchorApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorApp.swift
+//  Anchor
+//
+//  Created by James Birney on 24/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnchorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnchorTests/AnchorTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnchorTests.swift
+//  AnchorTests
+//
+//  Created by James Birney on 24/12/2025.
+//
+
+import Testing
+@testable import Anchor
+
+struct AnchorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
